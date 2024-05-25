900c45d8b4f93bcd0ca643ccde0cfb32cb10c4c3
PythonRF <105165044+PythonRF@users.noreply.github.com>
2024-05-25 22:34:26 +0300

    reform

--- handlers/users/text.py
+++ handlers/users/text.py
@@ -67,7 +67,7 @@ async def profile_handler_call(call: CallbackQuery):
         )
     await call.message.edit_media(
         InputMediaPhoto(
-            media=('https://telegra.ph/file/a0324f8b445c4b724dfcf.png'),
+            media=('https://telegra.ph/file/ba585ea63559091e3ac24.png'),
             caption=cabinet_msg.format(
                 user_id=call.from_user.id,
                 data=str(user.date)[:10],
